b0f565b26b64b3f99cfbbcbfabf8908d6961e0f1
Bangmaster <feelthepomp@gmail.com>
2026-05-23 00:36:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -857,56 +857,63 @@ function generujAlgorytm({talie,czlonkowie,posiadane,duplikaty,typWymiany,tryb,v
     }
   } else {
     // TRYB "PRIORYTET"
-    // Iteruj fazy po kolei od najważniejszej, w każdej fazie sortuj po nagrodzie.
-    // Dla każdej talii przydzielaj WSZYSTKIE brakujące karty (każda od innego dawcy).
-    // Dawca wysyła tylko 1 kartę, odbiorca może dostać wiele kart z różnych talii.
+    // Dwuprzebiegowy algorytm:
+    // Przebieg 1: dla każdej talii/osoby z brakT>1 zarezerwuj tylu dawców ile potrzeba
+    // Przebieg 2: reszta dawców przydzielana normalnie po fazach
 
-    const fazyPoKolei = [10,20,11,12,21,22,31,32,41,42,51,52];
+    // Zbierz wszystkie potrzeby pogrupowane po (osoba, talia)
+    const potrzebyGrup = staneTalii
+      .map(s => {
+        const efNagroda = obliczEfektywnaНagrode(s.osoba.id, s.talia.id, s.brakT.length);
+        const faza = obliczFaze(s.brakT.length, s.brakO.length, typWymiany);
+        const progInfo = progiOsob[s.osoba.id];
+        return { ...s, faza, efNagroda,
+          bliskoProg: !!(progInfo?.nastepnyProg && progInfo.brakujeDoProg <= 2),
+          progBonus: efNagroda - s.nagroda
+        };
+      })
+      .sort((a, b) => {
+        // Sortuj: faza → efektywna nagroda → trudna
+        if (a.faza !== b.faza) return a.faza - b.faza;
+        if (b.efNagroda !== a.efNagroda) return b.efNagroda - a.efNagroda;
+        if (!ignorujTrudne) { const aT=a.trudna?1:0,bT=b.trudna?1:0; if(aT!==bT) return aT-bT; }
+        return 0;
+      });
 
-    for (const fazaDocelowa of fazyPoKolei) {
-      const wTejFazie = staneTalii
-        .filter(s => obliczFaze(s.brakT.length, s.brakO.length, typWymiany) === fazaDocelowa)
-        .map(s => {
-          const efNagroda = obliczEfektywnaНagrode(s.osoba.id, s.talia.id, s.brakT.length);
-          const progInfo = progiOsob[s.osoba.id];
-          return { ...s, efNagroda, bliskoProg: !!(progInfo?.nastepnyProg && progInfo.brakujeDoProg <= 2), progBonus: efNagroda - s.nagroda };
-        })
-        .sort((a, b) => {
-          if (b.efNagroda !== a.efNagroda) return b.efNagroda - a.efNagroda;
-          if (!ignorujTrudne) { const aT=a.trudna?1:0,bT=b.trudna?1:0; if(aT!==bT) return aT-bT; }
-          return a.brakT.length - b.brakT.length;
-        });
+    for (const s of potrzebyGrup) {
+      for (const karta of s.brakT) {
+        const key = `${s.osoba.id}_${s.talia.id}_${karta.nazwa}`;
+        if (posiadane[key]) continue;
+        if (planoweWymiany.some(w =>
+          w.do === s.osoba.nazwa && w.talia === s.talia.nazwa && w.karta === karta.nazwa
+        )) continue;
 
-      for (const s of wTejFazie) {
-        for (const karta of s.brakT) {
-          const key = `${s.osoba.id}_${s.talia.id}_${karta.nazwa}`;
-          if (posiadane[key]) continue;
-          if (planoweWymiany.some(w => w.do===s.osoba.nazwa && w.talia===s.talia.nazwa && w.karta===karta.nazwa)) continue;
+        // Przelicz aktualną fazę uwzględniając już zaplanowane
+        const juzIdzie = planoweWymiany.filter(w =>
+          w.do === s.osoba.nazwa && w.talia === s.talia.nazwa
+        ).length;
+        const aktBrakT = Math.max(1, s.brakT.length - juzIdzie);
+        const aktFaza = obliczFaze(aktBrakT, s.brakO.length, typWymiany);
+        const aktEfNagroda = obliczEfektywnaНagrode(s.osoba.id, s.talia.id, aktBrakT);
 
-          const juzIdzie = planoweWymiany.filter(w => w.do===s.osoba.nazwa && w.talia===s.talia.nazwa).length;
-          const aktBrakT = Math.max(1, s.brakT.length - juzIdzie);
-          const aktFaza = obliczFaze(aktBrakT, s.brakO.length, typWymiany);
-          const aktEfNagroda = obliczEfektywnaНagrode(s.osoba.id, s.talia.id, aktBrakT);
+        let dawca = null;
+        for (const o2 of czlonkowie) {
+          if (o2.id === s.osoba.id || wysylajacy.has(o2.id)) continue;
+          if (duplikaty[`${o2.id}_${s.talia.id}_${karta.nazwa}`]) { dawca = o2; break; }
+        }
 
-          let dawca = null;
-          for (const o2 of czlonkowie) {
-            if (o2.id === s.osoba.id || wysylajacy.has(o2.id)) continue;
-            if (duplikaty[`${o2.id}_${s.talia.id}_${karta.nazwa}`]) { dawca = o2; break; }
-          }
-
-          if (dawca) {
-            wysylajacy.add(dawca.id);
-            planoweWymiany.push({
-              od: dawca.nazwa, do: s.osoba.nazwa,
-              karta: karta.nazwa, talia: s.talia.nazwa,
-              nagroda: s.nagroda, faza: aktFaza,
-              brakTCount: aktBrakT, brakOCount: s.brakO.length,
-              trudna: s.trudna, progBonus: aktEfNagroda - s.nagroda,
-              bliskoProg: s.bliskoProg||false,
-            });
-          } else if (s.brakT.length <= 3) {
-            nieobsluzone.push({ osoba: s.osoba, talia: s.talia, karta, brakTCount: s.brakT.length });
-          }
+        if (dawca) {
+          wysylajacy.add(dawca.id);
+          planoweWymiany.push({
+            od: dawca.nazwa, do: s.osoba.nazwa,
+            karta: karta.nazwa, talia: s.talia.nazwa,
+            nagroda: s.nagroda, faza: aktFaza,
+            brakTCount: aktBrakT, brakOCount: s.brakO.length,
+            trudna: s.trudna, progBonus: aktEfNagroda - s.nagroda,
+            bliskoProg: s.bliskoProg||false,
+          });
+        } else if (s.brakT.length <= 3) {
+          nieobsluzone.push({ osoba: s.osoba, talia: s.talia, karta, brakTCount: s.brakT.length });
         }
       }
     }
